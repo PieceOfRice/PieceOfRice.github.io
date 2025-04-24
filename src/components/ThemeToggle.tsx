ad94e27d00c565855ad23061faba97cacce447f2
Rhys <rxh293@student.bham.ac.uk>
2025-04-24 16:40:38 +0100

    base page + toggle theme

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const ThemeToggle = () => {
+
+    const [ isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () => {
+        const soundEffect = new Audio("/zapsplat_click.mp3");
+        soundEffect.volume = 0.6;
+        soundEffect.play();
+        setIsDarkMode((prevMode) => !prevMode)
+    };
+
+    
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.add('dark');
+            // localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark');
+            // localStorage.setItem('theme', 'light');
+        }
+        
+    }, [isDarkMode]);
+
+    return (
+        <button
+        className="p-2 bg-transparent border-none w-13 h-13 flex items-center justify-center z-10 active:scale-10 transition-transform duration-200"
+        onClick={toggleTheme}
+        >
+        <img src={isDarkMode ? "/sun.svg" : "/moon.svg"} alt="Toggle Theme" className="w-12 h-12" />
+        </button>
+    );
+};
+
+export default ThemeToggle;
